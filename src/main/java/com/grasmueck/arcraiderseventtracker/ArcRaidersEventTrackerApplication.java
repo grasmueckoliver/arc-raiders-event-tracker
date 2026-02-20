80b4ac479d7ad5c7b7d2422d5c059ef46d750d5d
Oliver Grasmück <grasmueck.oliver@gmail.com>
2026-02-20 09:50:17 +0100

    INIT - Spring Boot / Java 21 / Maven

---
+++ src/main/java/com/grasmueck/arcraiderseventtracker/ArcRaidersEventTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.grasmueck.arcraiderseventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcRaidersEventTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcRaidersEventTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grasmueck/arcraiderseventtracker/ArcRaidersEventTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grasmueck.arcraiderseventtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcRaidersEventTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
