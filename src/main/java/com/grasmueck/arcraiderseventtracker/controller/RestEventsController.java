0445a18315111c302a5d9f550a588e91534c3283
Oliver Grasmück <grasmueck.oliver@gmail.com>
2026-03-03 12:01:54 +0100

    - added comment

--- src/main/java/com/grasmueck/arcraiderseventtracker/controller/RestEventsController.java
+++ src/main/java/com/grasmueck/arcraiderseventtracker/controller/RestEventsController.java
@@ -11,8 +11,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-// This controller class defines REST endpoints for retrieving all events and filtered events based on event name and map name.
-// It uses the MetaforgeClient to fetch event data and the FilterEvents service to filter the events according to the specified criteria.
+/**
+ * This controller class defines REST endpoints for retrieving all events and filtered events based on event name and map name.
+ * It uses the MetaforgeClient to fetch event data and the FilterEvents service to filter the events according to the specified criteria.
+ **/
 @RestController
 @RequestMapping("/events")
 public class RestEventsController {
@@ -24,6 +26,7 @@ public class RestEventsController {
         this.repository = repository;
         this.filterEventsService = filterEventsService;
     }
+
 
     @GetMapping("/all")
     public List<MetaforgeEventDto> getAllEvents() {
@@ -37,6 +40,13 @@ public class RestEventsController {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * @param args an array of strings where each pair of elements represents an event name and a map name to filter by.
+     *             For example, args[0] is the first event name, args[1] is the first map name,
+     *             args[2] is the second event name, args[3] is the second map name, and so on.
+     * @return a list of FilteredEventsDto, each containing the event name, map name
+     * and a list of start times (hours of the day) for events matching the criteria.
+     */
     @GetMapping("/filtered")
     public List<FilteredEventsDto> getFilteredEvents(@RequestParam String[] args) {
 
@@ -59,7 +69,8 @@ public class RestEventsController {
                                 args[3],
                                 filterEventsService.filterEventsByStartTimes(args[2], args[3], data)));
 
-            } if (args.length > 4) {
+            }
+            if (args.length > 4) {
                 System.out.println("Filtering events for event name: " + args[4] + " and map name: " + args[5]);
                 filteredEvents.add(
                         new FilteredEventsDto(
@@ -67,7 +78,8 @@ public class RestEventsController {
                                 args[5],
                                 filterEventsService.filterEventsByStartTimes(args[4], args[5], data)));
 
-            } if (args.length > 6) {
+            }
+            if (args.length > 6) {
                 System.out.println("Filtering events for event name: " + args[6] + " and map name: " + args[7]);
                 filteredEvents.add(
                         new FilteredEventsDto(
